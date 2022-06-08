6049243b3cfb5f38228ce2284993d0487ba45b10
Tyler Reaves <tylersi93@gmail.com>
2022-06-07 23:52:40 -0400

    lint fixes

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,13 +1,13 @@
-import { useSession } from 'next-auth/react';
+// import { useSession } from 'next-auth/react';
 import useSwr from 'swr';
 import { SiteMenu } from '../components/SiteMenu';
 
 const fetcher = (url) => fetch(url).then((res) => res.json());
 
 export default function Index() {
-    const { data: session } = useSession();
+    // const { data: session } = useSession();
 
-    const { data, mutate } = useSwr(`/api/data`, fetcher);
+    const { data } = useSwr(`/api/data`, fetcher);
 
     return (
         <>
